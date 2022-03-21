6517fa0500f4bdfc2f8d3a7e70fe78c09c9b076d
Ozone <91910634+ozone3950@users.noreply.github.com>
2022-03-21 13:20:33 +0000

    Update not pokemon.py

--- not pokemon.py
+++ not pokemon.py
@@ -1,5 +1,7 @@
 import time
 
+# Remember to use https://fsymbols.com/draw/
+
 # The "Type Message" function
 letter_delay = 0.025
 message_delay = 0.5
@@ -9,14 +11,17 @@ def type_message(message):
         time.sleep(letter_delay)
     print()
     time.sleep(message_delay)
-    
+
+
+
 
 # Introduction to the game
 def intro():
+    type_message("Phone dialing...")
     type_message("PROF OAK:")
-    type_message("Hello there! Welcome to the world of Pokémon!")
+    type_message("*coughs* Oh- Hello there! Welcome to the world of Pokémon!")
     time.sleep(0.5)
-    type_message("My name is Oak! People call me the Pokémon Prof!")
+    type_message("My name is Oak! People call me the Pokémon Professor!")
     time.sleep(0.5)
     type_message("This world is inhabited by creatures called Pokémon!")
     time.sleep(0.5)
@@ -28,28 +33,52 @@ def intro():
     name = input(">>> ")
     type_message("Right! So your name is " + name + "!")
     time.sleep(0.5)
-    type_message("This is my grandson. He's been your rival since you were a baby.")
+    type_message("This is my great grandson. He's been your rival since you were a baby.")
     time.sleep(0.5)
     type_message("…Erm, what is his name again?")
     rival = input(">>> ")
     type_message("That's right! I remember now! His name is " + rival + "!")
     time.sleep(0.5)
-    type_message(name + "! Your very own Pokémon legend is about to unfold!")
+    type_message("I'm getting too old for this...")
+    time.sleep(0.5)
+    type_message("I mean- " + name + "! Your very own Pokémon legend is about to unfold!")
     time.sleep(0.5)
     type_message("A world of dreams and adventures with Pokémon awaits! Let's go!")
 
 
+def home_start():
+    type_message("Location: Home")
+    time.sleep(0.5)
+    type_message("Options:")
+    type_message("(1) Read Oak's note.")
+    type_message("(2) Exit home.")
+    home_start = input(">>> ")
+    if home_start == "1":
+        type_message("")
+
+
 # Titlescreen
 def titlescreen():
-    type_message("Not Pokémon")
-    type_message("PRESS START")
+    print('''
+      ___     _                        ___      _   _              __   __          _
+     | _ \___| |_____ _ __  ___ _ _   | _ \_  _| |_| |_  ___ _ _   \ \ / /__ _ _ __(_)___ _ _
+     |  _/ _ \ / / -_) '  \/ _ \ ' \  |  _/ || |  _| ' \/ _ \ ' \   \ V / -_) '_(_-< / _ \ ' \\
+     |_| \___/_\_\___|_|_|_\___/_||_| |_|  \_, |\__|_||_\___/_||_|   \_/\___|_| /__/_\___/_||_|
+                                           |__/
+    
+                                        PRESS START''')
     start = input("")
     if start == "":
+        print('''
+
+[==================================================================================================]
+        ''')
         intro()
     else:
-        print("")
-        print("")
-        
+        print('''
+
+[==================================================================================================]
+        ''')
         titlescreen()
 
 
