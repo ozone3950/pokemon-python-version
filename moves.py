57fe373348de9584b22ae6065257337ac02e08db
Ozone <91910634+ozone3950@users.noreply.github.com>
2022-03-31 17:36:01 +0100

    Create moves.py

---
+++ moves.py
@@ -0,0 +1,21 @@
+Tackle = {
+    "Type": "Normal",
+    "Category": "Physical",
+    "PP": 35,
+    "Power": 40,
+    "Accuracy": 100,
+}
+
+
+
+"""
+Template
+
+Move = {
+    "Type":
+    "Category":
+    "PP":
+    "Power":
+    "Accuracy":
+}
+"""
