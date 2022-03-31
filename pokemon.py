fcfd1f15b1cc2c9ac3df09e32dae7a2d1925149d
Ozone <91910634+ozone3950@users.noreply.github.com>
2022-03-31 17:26:45 +0100

    Create pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,52 @@
+Bulbasaur = {
+    "dexnum": 001,
+    "type": "Grass" "Poison",
+    "evolvl": 16,
+    "hp": 45,
+    "atk": 49,
+    "def": 49,
+    "satk": 65,
+    "sdef": 65,
+    "spd": 45
+}
+
+Ivysaur = {
+    "dexnum": 002,
+    "type": "Grass" "Poison",
+    "evolvl": 32,
+    "HP": 60,
+    "ATK": 62,
+    "DEF": 63,
+    "sATK": 80,
+    "sDEF": 80,
+    "SPD": 60
+}
+Venusaur = {
+    "dexnum": 003,
+    "type": "Grass" "Poison",
+    "evolvl": "N/A",
+    "hp": 80,
+    "atk": 82,
+    "def": 83,
+    "satk": 100,
+    "sdef": 100,
+    "spd": 80
+
+
+
+
+"""
+Template
+
+Pokemon = {
+    "dexnum":
+    "type":
+    "evolvl":
+    "hp":
+    "atk":
+    "def":
+    "satk":
+    "sdef":
+    "spd":
+}
+"""
