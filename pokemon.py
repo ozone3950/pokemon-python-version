33c44071262251fd95fc4ea89f097087d6180d08
Ozone <91910634+ozone3950@users.noreply.github.com>
2022-04-01 10:41:34 +0100

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,20 @@
+?????????? = {
+    "dexnum": "000",
+    "species": "??????????",
+    "type": "999",
+    "evolvl": "N/A,
+    "hp": 255,
+    "atk":255,
+    "def": 255,
+    "satk": 255,
+    "sdef": 255,
+    "spd": 255
+    }
+
+
 Bulbasaur = {
-    "dexnum": 001,
+    "dexnum": "001",
+    "species": "Seed",
     "type": "Grass" "Poison",
     "evolvl": 16,
     "hp": 45,
@@ -11,7 +26,8 @@ Bulbasaur = {
 }
 
 Ivysaur = {
-    "dexnum": 002,
+    "dexnum": "002",
+    "species": "Seed",
     "type": "Grass" "Poison",
     "evolvl": 32,
     "HP": 60,
@@ -21,8 +37,10 @@ Ivysaur = {
     "sDEF": 80,
     "SPD": 60
 }
+
 Venusaur = {
-    "dexnum": 003,
+    "dexnum": "003",
+    "species": "Seed",
     "type": "Grass" "Poison",
     "evolvl": "N/A",
     "hp": 80,
@@ -31,22 +49,103 @@ Venusaur = {
     "satk": 100,
     "sdef": 100,
     "spd": 80
+}
+
+Charmander = {
+    "dexnum": "004",
+    "species": "Lizard",
+    "type": "Fire",
+    "evolvl": 16,
+    "hp": 39,
+    "atk": 42,
+    "def": 43,
+    "satk": 60,
+    "sdef": 50,
+    "spd": 65
+}
+
+Charmeleon = {
+    "dexnum": "005",
+    "species": "Flame",
+    "type": "Fire",
+    "evolvl": 32,
+    "hp": 58,
+    "atk": 64,
+    "def": 58,
+    "satk": 80,
+    "sdef": 65,
+    "spd": 80
+}
+
+Charizard = {
+    "dexnum": "006",
+    "species": "Flame",
+    "type": "Fire" "Flying"
+    "evolvl": "N?A"
+    "hp": 78,
+    "atk": 84,
+    "def": 78,
+    "satk": 109,
+    "sdef": 85,
+    "spd": 100
+}
+
+Squirtle = {
+    "dexnum": "007",
+    "species": "Tiny Turtle",
+    "type": "Water",
+    "evolvl": 16,
+    "hp": 44,
+    "atk": 48,
+    "def": 65,
+    "satk": 50,
+    "sdef": 64,
+    "spd": 43
+}
+
+Wartortle = {
+    "dexnum": "008",
+    "species": "Turtle",
+    "type": "Water",
+    "evolvl": 32,
+    "hp": 59,
+    "atk": 63,
+    "def": 80,
+    "satk": 65,
+    "sdef": 80,
+    "spd": 58
+}
+
+Blastoise = {
+    "dexnum": "009"
+    "species": "Shellfish",
+    "type": "Water",
+    "evolvl": "N/A",
+    "hp": 79,
+    "atk": 83,
+    "def": 100,
+    "satk": 85,
+    "sdef": 105,
+    "spd": 78
+}
 
 
 
 
-"""
+
+'''
 Template
 
 Pokemon = {
     "dexnum":
-    "type":
-    "evolvl":
-    "hp":
-    "atk":
-    "def":
-    "satk":
-    "sdef":
-    "spd":
+    "species": 
+    "type": 
+    "evolvl": 
+    "hp": 
+    "atk": 
+    "def": 
+    "satk": 
+    "sdef": 
+    "spd": 
 }
-"""
+'''
